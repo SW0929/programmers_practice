ec7c067072f1db1537a30566409935c220f97570
sw <sjugas1234@naver.com>
2023-06-01 22:38:11 +0900

    int_sprial_ing

---
+++ src/practice_Kotlin/int_spiral.kt
@@ -0,0 +1,20 @@
+package practice_Kotlin
+
+class int_spiral {
+}
+fun main(){
+    val n = 4
+    println(solution(n))
+}
+fun solution(n: Int): Array<IntArray> {
+    var answer: Array<IntArray> = arrayOf<IntArray>()
+    var count = 1
+
+    for (row in 0 until n){
+        for (col in 0 until n){
+            answer[row][col].plus(count)
+            count++
+        }
+    }
+    return answer
+}
