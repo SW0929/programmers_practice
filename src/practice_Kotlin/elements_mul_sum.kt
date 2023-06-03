b6bc35ff3ba77e58f2e4096360b514e7021f275d
sw <sjugas1234@naver.com>
2023-06-03 20:07:18 +0900

    elements_mul_sum

---
+++ src/practice_Kotlin/elements_mul_sum.kt
@@ -0,0 +1,22 @@
+package practice_Kotlin
+
+import kotlin.math.pow
+
+class elements_mul_sum {
+}
+fun main(){
+    val intArray = intArrayOf(3,4,5,2,1)
+    println(solution(intArray))
+}
+
+fun solution(num_list: IntArray): Int {
+    var answer: Int = 0
+    var mul = 1
+    num_list.forEach { mul *= it }
+    answer = if (num_list.sum().toDouble().pow(2.0) > mul){
+            1
+        }else{
+            0
+    }
+    return answer
+}
