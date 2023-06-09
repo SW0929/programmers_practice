303b7cf37ad110a1e17b2315bb6821171594f174
sw <sjugas1234@naver.com>
2023-06-09 21:26:20 +0900

    change_ll

---
+++ src/practice_Java/change_ll.java
@@ -0,0 +1,12 @@
+package practice_Java;
+
+public class change_ll {
+    public static void main(String[] args) {
+        System.out.println(solution("aBcDeFg"));
+    }
+    public static String solution(String myString) {
+        String answer = "";
+        answer = myString.toUpperCase();
+        return answer;
+    }
+}
