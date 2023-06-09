d645e10a7c24c38b2a4f307a5bf95bebab8f0a3d
sw <sjugas1234@naver.com>
2023-06-09 21:23:05 +0900

    change_small_letter

---
+++ src/practice_Java/change_sl.java
@@ -0,0 +1,16 @@
+package practice_Java;
+
+import java.util.stream.IntStream;
+
+public class change_sl {
+    public static void main(String args[]){
+
+        System.out.println(solution("aBcDeFg"));
+    }
+
+    public static String solution(String myString) {
+        String answer = "";
+        answer = myString.toLowerCase();
+        return answer;
+    }
+}
