2a28594a68fc1c8e31deada35ceac0cb7aad0bde
sw <sjugas1234@naver.com>
2023-06-13 20:27:16 +0900

    06/13/23_05

---
+++ src/practice_Java/dice_game.java
@@ -0,0 +1,18 @@
+package practice_Java;
+
+public class dice_game {
+    public static void main(String[] args) {
+        System.out.println(diceGameSolution(3,5));
+    }
+    public static int diceGameSolution(int a, int b) {
+        int answer = 0;
+        if (a % 2 != 0 && b % 2 !=0){
+            answer = (a*a)+(b*b);
+        }else if (a % 2 != 0 || b % 2 !=0){
+            answer = 2 * (a + b);
+        }else{
+            answer = Math.abs(a - b);
+        }
+        return answer;
+    }
+}
