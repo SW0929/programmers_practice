e4ce8277050ff4191de8a51d8de884d1f76143dd
SangWon Lee <97885055+SW0929@users.noreply.github.com>
2023-06-15 12:45:50 +0900

    06/15/23_01

--- src/practice_Kotlin/ipad_practice_kt.kt
+++ src/practice_Kotlin/ipad_practice_kt.kt
@@ -5,3 +5,17 @@ class ipad_practice_kt {
 fun main(){
 
 }
+// 카운트 다운
+/*
+fun countDownSolution(start: Int, end: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        for(i in start downTo end){
+            answer = answer.plus(i)
+        }
+        return answer
+    }
+fun countDownSolution02(start: Int, end: Int) = (end..start).toList().sortedDescending()
+
+fun countDownSolution03(start: Int, end: Int): IntArray = (start downTo end).toList().toIntArray()
+*/
+
