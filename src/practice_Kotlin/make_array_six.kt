115a1a9574fa931c5d9d5697596b4c4cbc4f9a3b
sw <sjugas1234@naver.com>
2023-07-13 17:33:34 +0900

    07/13/23_04

--- src/practice_Kotlin/make_array_six.kt
+++ src/practice_Kotlin/make_array_six.kt
@@ -1,5 +1,7 @@
 package practice_Kotlin
 
+import java.util.*
+
 class make_array_six {
 }
 fun main(){
@@ -22,4 +24,19 @@ fun makeArraySixSolution(arr: IntArray): IntArray {
         answer.add(-1)
     }
     return answer.toIntArray()
+}
+fun makeArraySixSolution02(arr: IntArray): IntArray {
+    val stk = Stack<Int>()
+
+    for (i in arr.indices) {
+        if (stk.isEmpty()) {
+            stk.push(arr[i])
+        } else if (stk.peek() == arr[i]) {
+            stk.pop()
+        } else {
+            stk.push(arr[i])
+        }
+    }
+    if (stk.isEmpty()) stk.push(-1)
+    return stk
 }
